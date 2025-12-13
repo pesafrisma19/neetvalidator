d7d9782556a186c4e67957225328d8eb18bd0fd5
pesafrisma19 <pesafrismaagustina@gmail.com>
2025-12-13 12:52:34 +0700

    Update mlbb.js

--- assets/js/games/mlbb.js
+++ assets/js/games/mlbb.js
@@ -1,7 +1,6 @@
 export async function checkMLBB(userId, serverId) {
-  const res = await fetch(
-    `/api/mlbb?user_id=${userId}&server_id=${serverId}`
-  );
-
-  return await res.json();
+  return {
+    success: true,
+    username: 'DummyUser'
+  };
 }
